952fd3dac28816071fe5d77fa2d9c4d99dab91f0
Larisa <laserova@yandex.ru>
2023-06-13 00:22:55 +0300

    first version - incorrectly output

---
+++ src/hw12/Book.java
@@ -0,0 +1,37 @@
+package hw12;
+
+public class Book {
+    private String bookName;
+    private Author authorName;
+    private int publishingYear;
+
+    public Book(String bookName, Author authorName, int publishingYear) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public Author getAuthorName() {
+        return this.authorName;
+    }
+
+    public int getPublishingYear() {
+        return this.publishingYear;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public void setAuthorName(Author authorName) {
+        this.authorName = authorName;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+}
